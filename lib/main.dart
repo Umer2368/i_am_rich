aa7a11f7e7255a1fc34c1529728e51d88fdbdaa2
Umer <fumer2368@gmail.com>
2020-10-18 16:22:15 +0500

    fumer2368@gmail.com

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              ('Peace is in the air'),
+            ),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
